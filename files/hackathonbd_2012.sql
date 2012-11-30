69c05d6edcbd9a90b266ce7a4bdf3dd165bf691a
Md Imran Hasan Hira <imranhasanhira@gmail.com>
2012-12-01 02:42:05 +0600

    sql added

--- files/hackathonbd_2012.sql
+++ files/hackathonbd_2012.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2012 at 08:27 PM
+-- Generation Time: Nov 30, 2012 at 08:41 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -25,6 +25,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `attendance`
 --
 
+DROP TABLE IF EXISTS `attendance`;
 CREATE TABLE IF NOT EXISTS `attendance` (
   `meeting_id` int(11) NOT NULL,
   `user_id` varchar(50) NOT NULL,
@@ -42,6 +43,7 @@ CREATE TABLE IF NOT EXISTS `attendance` (
 -- Table structure for table `committee_member`
 --
 
+DROP TABLE IF EXISTS `committee_member`;
 CREATE TABLE IF NOT EXISTS `committee_member` (
   `union_id` int(11) NOT NULL,
   `user_id` varchar(50) NOT NULL,
@@ -52,6 +54,10 @@ CREATE TABLE IF NOT EXISTS `committee_member` (
 -- Dumping data for table `committee_member`
 --
 
+INSERT INTO `committee_member` (`union_id`, `user_id`) VALUES
+(2447110, 'sdgsdg'),
+(2447183, '1234'),
+(2447183, 'eoifqeoif');
 
 -- --------------------------------------------------------
 
@@ -59,6 +65,7 @@ CREATE TABLE IF NOT EXISTS `committee_member` (
 -- Table structure for table `department`
 --
 
+DROP TABLE IF EXISTS `department`;
 CREATE TABLE IF NOT EXISTS `department` (
   `department_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text,
@@ -76,6 +83,7 @@ CREATE TABLE IF NOT EXISTS `department` (
 -- Table structure for table `department_report`
 --
 
+DROP TABLE IF EXISTS `department_report`;
 CREATE TABLE IF NOT EXISTS `department_report` (
   `meeting_id` int(11) NOT NULL,
   `department_id` int(11) NOT NULL,
@@ -96,6 +104,7 @@ CREATE TABLE IF NOT EXISTS `department_report` (
 -- Table structure for table `district`
 --
 
+DROP TABLE IF EXISTS `district`;
 CREATE TABLE IF NOT EXISTS `district` (
   `district_id` int(11) NOT NULL AUTO_INCREMENT,
   `division_id` int(11) DEFAULT NULL,
@@ -114,6 +123,7 @@ CREATE TABLE IF NOT EXISTS `district` (
 -- Table structure for table `division`
 --
 
+DROP TABLE IF EXISTS `division`;
 CREATE TABLE IF NOT EXISTS `division` (
   `division_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text,
@@ -131,6 +141,7 @@ CREATE TABLE IF NOT EXISTS `division` (
 -- Table structure for table `meeting`
 --
 
+DROP TABLE IF EXISTS `meeting`;
 CREATE TABLE IF NOT EXISTS `meeting` (
   `meeting_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
@@ -151,6 +162,7 @@ CREATE TABLE IF NOT EXISTS `meeting` (
 -- Table structure for table `union`
 --
 
+DROP TABLE IF EXISTS `union`;
 CREATE TABLE IF NOT EXISTS `union` (
   `union_code` int(11) NOT NULL,
   `upazila_id` int(11) DEFAULT NULL,
@@ -169,6 +181,7 @@ CREATE TABLE IF NOT EXISTS `union` (
 -- Table structure for table `upazila`
 --
 
+DROP TABLE IF EXISTS `upazila`;
 CREATE TABLE IF NOT EXISTS `upazila` (
   `upazila_id` int(11) NOT NULL AUTO_INCREMENT,
   `district_id` int(11) DEFAULT NULL,
@@ -187,11 +200,13 @@ CREATE TABLE IF NOT EXISTS `upazila` (
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` varchar(50) NOT NULL,
   `name` text,
   `role` text,
   `password` text,
+  `email` text NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -199,7 +214,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `name`, `role`, `password`) VALUES
-('1234', NULL, 'asdaisdas', 'a5cec7af5f7aab769cf0d4aa440e01c7bfc371b2'),
-('eoifqeoif', 'dummy', 'asfa', 'sdgsd'),
-('sdgsdg', 'sdgsdg', 'sdgsd', 'sdgsdg');
+INSERT INTO `user` (`user_id`, `name`, `role`, `password`, `email`) VALUES
+('1234', NULL, 'asdaisdas', 'a5cec7af5f7aab769cf0d4aa440e01c7bfc371b2', ''),
+('eoifqeoif', 'dummy', 'asfa', 'sdgsd', ''),
+('sdgsdg', 'sdgsdg', 'sdgsd', 'sdgsdg', '');
