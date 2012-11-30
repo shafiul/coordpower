c7785cf746810622699baf64d34d1fdcb71d7888
Md Imran Hasan Hira <imranhasanhira@gmail.com>
2012-12-01 02:40:39 +0600

    committee_member model added

---
+++ files/hackathonbd_2012.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 30, 2012 at 08:27 PM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `hackathonbd_2012`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `meeting_id` int(11) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  PRIMARY KEY (`meeting_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `attendance`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `committee_member`
+--
+
+CREATE TABLE IF NOT EXISTS `committee_member` (
+  `union_id` int(11) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  PRIMARY KEY (`union_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `committee_member`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE IF NOT EXISTS `department` (
+  `department_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  PRIMARY KEY (`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `department`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department_report`
+--
+
+CREATE TABLE IF NOT EXISTS `department_report` (
+  `meeting_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `discussion` text,
+  `decision` text,
+  `responsiblee` text,
+  PRIMARY KEY (`meeting_id`,`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department_report`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `district`
+--
+
+CREATE TABLE IF NOT EXISTS `district` (
+  `district_id` int(11) NOT NULL AUTO_INCREMENT,
+  `division_id` int(11) DEFAULT NULL,
+  `name` text,
+  PRIMARY KEY (`district_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `district`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `division`
+--
+
+CREATE TABLE IF NOT EXISTS `division` (
+  `division_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  PRIMARY KEY (`division_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `division`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meeting`
+--
+
+CREATE TABLE IF NOT EXISTS `meeting` (
+  `meeting_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `time` timestamp NULL DEFAULT NULL,
+  `president` text,
+  `place` text,
+  PRIMARY KEY (`meeting_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `meeting`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `union`
+--
+
+CREATE TABLE IF NOT EXISTS `union` (
+  `union_code` int(11) NOT NULL,
+  `upazila_id` int(11) DEFAULT NULL,
+  `name` text,
+  PRIMARY KEY (`union_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `union`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upazila`
+--
+
+CREATE TABLE IF NOT EXISTS `upazila` (
+  `upazila_id` int(11) NOT NULL AUTO_INCREMENT,
+  `district_id` int(11) DEFAULT NULL,
+  `name` text,
+  PRIMARY KEY (`upazila_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `upazila`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` varchar(50) NOT NULL,
+  `name` text,
+  `role` text,
+  `password` text,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `name`, `role`, `password`) VALUES
+('1234', NULL, 'asdaisdas', 'a5cec7af5f7aab769cf0d4aa440e01c7bfc371b2'),
+('eoifqeoif', 'dummy', 'asfa', 'sdgsd'),
+('sdgsdg', 'sdgsdg', 'sdgsd', 'sdgsdg');
