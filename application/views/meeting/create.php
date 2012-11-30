b12a264122797affa4370dea777c696c058f452b
Ibrahim Rashid <ibrahim12@gmail.com>
2012-12-01 04:50:05 +0600

    sdf

---
+++ application/views/meeting/create.php
@@ -0,0 +1,25 @@
+
+<div class="span10">
+
+    <div class="row-fluid">
+        
+        <br />
+        
+        <form class="form-signin">
+            
+            <h2 class="form-signin-heading">Create Meeting</h2>
+            
+            <input name="date" type="text" class="input-block-level" placeholder="Date">
+            <input name="email" type="text" class="input-block-level" placeholder="Email address">
+            
+            <input name="passwd" type="password" class="input-block-level" placeholder="Password">
+            <input name="passwd2" type="password" class="input-block-level" placeholder="Retype Password">
+            
+            <input name="upcode" type="text" class="input-block-level" placeholder="Union Parishad Code">
+            
+            
+            <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+        </form>
+    </div>
+
+</div>
