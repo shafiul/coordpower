8d5b5b5081953dcd1629e19c35061474d1fda769
Md Imran Hasan Hira <imranhasanhira@gmail.com>
2012-12-01 01:23:18 +0600

    database.sql added to "files/" database.config changed to work with "hirayami-HP" pc

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,11 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+if(gethostname() == 'hirayami-HP'){
+    $db['default']['username'] = 'root';
+    $db['default']['password'] = '';
+}
+$db['default']['database'] = 'hackathonbd_2012';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
