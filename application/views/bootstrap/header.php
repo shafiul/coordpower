5e01e967daffaa1e7f927e275e73c34e8549e5bf
Shafiul Azam <ishafiul@gmail.com>
2012-12-01 10:21:45 +0600

    dfsd

--- application/views/bootstrap/header.php
+++ application/views/bootstrap/header.php
@@ -9,7 +9,7 @@
 
         <!-- Le styles -->
         <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
-        <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
+        
         <style type="text/css">
             body {
                 padding-top: 60px;
@@ -23,6 +23,7 @@
             var base_url = "<?php echo base_url(); ?>";
         </script>
         <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
 
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
